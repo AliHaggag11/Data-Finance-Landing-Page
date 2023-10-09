939b3ce9edee69efbbd8b476c7e32cd16fb0fe96
AliHaggag11 <Ali7aggag@gmail.com>
2023-10-09 06:19:16 +0300

    first

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import laptop from '../assets/laptop.jpg'
+
+const Analytics = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+            <img className='w-[500px] mx-auto my-4' src={laptop} alt='/' />
+            <div className=' flex flex-col justify-center'>
+                <p className='text-[#00df9a] font-bold'>DATA ANALYTICS DASHBOARD</p>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+                <p>Managing data analytics centrally provides a holistic view of data, improves quality and consistency, facilitates collaboration, reduces costs, and increases scalability and agility.</p>
+                <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button>            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Analytics
---
+++ src/components/Cards.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Single from '../assets/single.png'
+import Double from '../assets/double.png'
+import Triple from '../assets/triple.png'
+
+const Cards = () => {
+  return (
+    <div className='w-full py-[10rem] px-4 bg-white'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
+          <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
+              <h2 className='text-2xl font-bold text-center py-8'>Single Plan</h2>
+              <p className='text-center text-4xl font-bold'>$149</p>
+              <div className='text-center font-medium'>
+                  <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
+                  <p className='py-2 border-b mx-8'>1 Granted User</p>
+                  <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
+              </div>
+              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+          </div>
+          <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
+              <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
+              <h2 className='text-2xl font-bold text-center py-8'>Duo Plan</h2>
+              <p className='text-center text-4xl font-bold'>$250</p>
+              <div className='text-center font-medium'>
+                  <p className='py-2 border-b mx-8 mt-8'>1 TB Storage</p>
+                  <p className='py-2 border-b mx-8'>2 Granted Users</p>
+                  <p className='py-2 border-b mx-8'>Send up to 5 GB</p>
+              </div>
+              <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+          </div>
+          <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
+              <h2 className='text-2xl font-bold text-center py-8'>Enterprise Plan</h2>
+              <p className='text-center text-4xl font-bold'>$450</p>
+              <div className='text-center font-medium'>
+                  <p className='py-2 border-b mx-8 mt-8'>3 GB Storage</p>
+                  <p className='py-2 border-b mx-8'>3 Granted Users</p>
+                  <p className='py-2 border-b mx-8'>Send up to 15 GB</p>
+              </div>
+              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
